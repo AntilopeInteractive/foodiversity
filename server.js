c4a71f0a7a41cf50ebd473d7b5d98293d14af2c1
AntilopeInteractive <antilopeinteractive@gmail.com>
2017-11-17 16:41:46 -0500

    deply final

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const port = process.env.PORT || 8080
 
-mongoose.connect('mongodb://foodiversity:foodiversity@ds125255.mlab.com:25255/foodiversity', {useMongoClient: true})
-//mongoose.connect('mongodb://127.0.0.1:27017/foodiversity', {useMongoClient: true})
+//mongoose.connect('mongodb://foodiversity:foodiversity@ds125255.mlab.com:25255/foodiversity', {useMongoClient: true})
+mongoose.connect('mongodb://127.0.0.1:27017/foodiversity', {useMongoClient: true})
 
 app.listen(port, () => {
   console.log(`servidor foodiversity ${port}`)
